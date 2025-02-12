171b55910a36365017be714c104301b9b971073e
oneHFR <577612497@qq.com>
2025-02-12 16:01:59 +0800

    bbb

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,32 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
+    "Shanghai, China",
+    31.230416,
+    121.473701
+  ],
+  [
+    "Vienna, Austria",
+    48.2081743,
+    16.3738189
+  ],
+  [
+    "Munich, Germany",
+    48.1351253,
+    11.5819805
+  ],
+  [
+    "Berkeley, CA, USA",
     37.8708393,
     -122.2728638
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Stanford, CA, USA",
+    37.4274745,
+    -122.169719
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Sacramento, CA, USA",
+    38.5815719,
+    -121.4943996
   ]
 ];
